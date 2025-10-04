c7a4e7f05f4f68c243f63001dc9aeefd7beb93ec
Samuel Dario <sssamuelll@gmail.com>
2025-10-04 12:39:06 +0000

    added base to config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/diffviewer/',
+  build: {
+    outDir: './dist'
+  },
   plugins: [react()],
 })
